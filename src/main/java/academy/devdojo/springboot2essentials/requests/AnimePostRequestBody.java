b4658391afd2c5471b59bdef2f432e9964d17a29
William Bigas Mauro <williambmauro@hotmail.com>
2022-04-25 12:04:35 -0300

    Adicionado bean validation para validacao dos Requests DTOs

--- src/main/java/academy/devdojo/springboot2essentials/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2essentials/requests/AnimePostRequestBody.java
@@ -2,9 +2,12 @@ package academy.devdojo.springboot2essentials.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
 
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 
 
