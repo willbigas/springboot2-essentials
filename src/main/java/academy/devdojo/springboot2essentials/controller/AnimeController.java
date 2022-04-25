b3fb166ac1e7062e89f2319db9695d10113bc4eb
William Bigas Mauro <williambmauro@hotmail.com>
2022-04-25 14:17:57 -0300

    Adicionado paginação no endpoint

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -7,6 +7,8 @@ import academy.devdojo.springboot2essentials.service.AnimeService;
 import academy.devdojo.springboot2essentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,8 +26,8 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<List<Anime>> findAll() {
-        return ResponseEntity.ok(animeService.findAll());
+    public ResponseEntity<Page<Anime>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(animeService.findAll(pageable));
     }
 
     @GetMapping(path = "/{id}")
