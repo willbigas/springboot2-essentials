ccb1444897d5cc5afcf39a58060d809a56427d89
William Bigas Mauro <williambmauro@hotmail.com>
2022-05-06 12:14:41 -0300

    Criado metodo com preauthorize para testar authorization do spring

--- src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
@@ -2,25 +2,35 @@ package academy.devdojo.springboot2essentials.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    /**
+     * BasicAuthenticationFilter (Base64 Crypt)
+     * UsernamePasswordAuthenticationFilter (User and Password)
+     * DefaultLoginPageGeneratingFilter (Page Login)
+     * DefaultLogoutPageGeneratingFilter (Page Logout)
+     * FilterSecurityInterceptor (Check if authorize)
+     * Authentication -> Authorization
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+        http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
+                .and()
+                .formLogin()
                 .and()
                 .httpBasic();
     }
